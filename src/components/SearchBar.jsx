baa3ec2a8390f9ae774a314cbab4bfd30c293a98
luis.campos <gucampos845@gmail.com>
2020-06-26 17:35:58 -0300

    Style page Comidas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/styles.css'
 
 function SearchBar() {
   return (
@@ -7,31 +8,40 @@ function SearchBar() {
         type="search"
         data-testid="search-input"
         placeholder="Buscar Receita"
-      />
-      <input
-        type="radio"
-        data-testid="ingredient-search-radio"
-        name="food"
-        id="Ingrediente"
-        value="Ingrediente"
-      />
-      <label htmlFor="Ingrediente">ingrediente</label>
-      <input
-        type="radio"
-        data-testid="name-search-radio"
-        name="food"
-        id="Name"
-        value="Name"
-      />
-      <label htmlFor="Name">Nome</label>
-      <input
-        type="radio"
-        data-testid="first-letter-search-radio"
-        name="food"
-        id="Fist-Word"
-        value="Fist-Word"
-      />
-      <label htmlFor="Fist-Word">Primeira letra</label>
+        className="Input-Search"
+      /><br />
+      <div className="Radio">
+        <label htmlFor="Ingrediente">
+          <input
+            type="radio"
+            data-testid="ingredient-search-radio"
+            name="food"
+            id="Ingrediente"
+            value="Ingrediente"
+          />
+          ingrediente
+        </label>
+        <label htmlFor="Name">
+          <input
+            type="radio"
+            data-testid="name-search-radio"
+            name="food"
+            id="Name"
+            value="Name"
+          />
+          Nome
+        </label>
+        <label htmlFor="Fist-Word">
+          <input
+            type="radio"
+            data-testid="first-letter-search-radio"
+            name="food"
+            id="Fist-Word"
+            value="Fist-Word"
+          />
+          Primeira letra
+        </label>
+      </div>
     </div>
   );
 }
