eddbc229c78595d04bd6dd45f4045b6deea9fafa
luizfernando <luizfernandotic@gmail.com>
2020-06-29 15:09:56 -0300

    implementação da chamada de api.

---
+++ src/components/Meals.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import ComidasContext from '../context/ComidasContext';
+import Loading from './Loading';
+import '../styles/styles.css';
+import { searchMealsByName } from '../services/requestMealApi';
+import MealsCard from './MealsCard';
+
+function Meals({ type }) {
+  const { searchValue, meals, setMeals, isFetching, setIsFetching } = useContext(ComidasContext);
+
+  useEffect(() => {
+    searchMealsByName('', type).then((data) => {
+      setMeals((data.meals || data.drinks).slice(0, 12));
+      setIsFetching(false);
+    });
+  }, [type]);
+
+  const upperCase = {
+    meal: 'Meal',
+    cocktail: 'Drink',
+  };
+
+  if (isFetching) return <Loading />;
+  return (
+    <div>
+      <Header title="Comidas" search />
+      {searchValue && <SearchBar />}
+      <div className="Map">
+        {meals.map((meal, index) => (
+          <MealsCard key={meal[`id${upperCase[type]}`]} recipe={meal} index={index} type={type} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+Meals.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default Meals;
