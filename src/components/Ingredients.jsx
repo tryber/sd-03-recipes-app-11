6d65b505dc043b6a334d1d6f1244b571ca7df3c8
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-06-30 05:14:26 -0300

    cc4

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Ingredients ({ value: { ingredients } }) {
+function Ingredients ({ value: { ingredients } }){
   // console.log(value.ingredients)
   return ingredients === undefined ? <div>looding...</div> : ingredients
     .map((el, index) => (
-    <p
-      key={el[0]}
-      data-testid={`${index}-ingredient-name-and-measure`}
-    >
-      {`${el[0]} - ${el[1]}`}
-    </p>));
-};
+      <p
+        key={el[0]}
+        data-testid={`${index}-ingredient-name-and-measure`}
+      >
+        {`${el[0]} - ${el[1]}`}
+      </p>));
+}
 
 Ingredients.propTypes = {
   value: PropTypes.shape({
     ingredients: PropTypes.array.isRequired,
-  }),
-}
+  }).isRequired,
+};
 
 export default Ingredients;
