320b20226ca5e3359e438f970afd61f0c1d2d9b2
ilan274 <ilan.herbach@gmail.com>
2020-07-01 18:33:54 -0300

    Header spec bar done

--- src/components/SearchBox/SearchButton.jsx
+++ src/components/SearchBox/SearchButton.jsx
@@ -3,13 +3,16 @@ import { useHistory } from 'react-router-dom';
 import ComidasContext from '../../context/ComidasContext';
 import * as api from '../../services/requestMealApi';
 
-const requestMeal = (type, meal, radio, push) => {
+const requestMeal = (type, meal, radio, push, setMeals) => {
   switch (radio) {
     case 'ingredient':
       return api.searchMealByMainIngredient(meal, type);
     case 'name':
       return api.searchMealsByName(meal, type)
-        .then((resp) => (resp.meals.length === 1) && push(`/comidas/${resp.meals[0].idMeal}`));
+        .then((resp) => (resp.meals.length === 1
+          ? push(`/comidas/${resp.meals[0].idMeal}`)
+          : setMeals(resp.meals)))
+        .catch(() => alert('Sinto muito, não encontramos nenhuma receita para esses filtros.'));
     case 'firstLetter':
       return api.searchAllMealsByFirstLetter(meal, type);
     default:
@@ -17,13 +20,16 @@ const requestMeal = (type, meal, radio, push) => {
   }
 };
 
-const requestCocktail = (type, meal, radio, push) => {
+const requestCocktail = (type, meal, radio, push, setMeals) => {
   switch (radio) {
     case 'ingredient':
       return api.searchMealByMainIngredient(meal, type);
     case 'name':
       return api.searchMealsByName(meal, type)
-        .then((resp) => (resp.drinks.length === 1) && push(`/bebidas/${resp.drinks[0].idDrink}`));
+        .then((resp) => (resp.drinks.length === 1
+          ? push(`/bebidas/${resp.drinks[0].idDrink}`)
+          : setMeals(resp.drinks.splice(0, 12))))
+        .catch(() => alert('Sinto muito, não encontramos nenhuma receita para esses filtros.'));
     case 'firstLetter':
       return api.searchAllMealsByFirstLetter(meal, type);
     default:
@@ -31,22 +37,22 @@ const requestCocktail = (type, meal, radio, push) => {
   }
 };
 
-const requestType = (type, meal, radio, pathname, push) => {
+const requestType = (type, meal, radio, pathname, push, setMeals) => {
   const alertMessage = 'Sua busca deve conter somente 1 (um) caracter';
 
   if (radio === 'firstLetter' && meal.length > 1) return alert(alertMessage);
   switch (pathname) {
     case '/comidas':
-      return requestMeal('meal', meal, radio, push);
+      return requestMeal('meal', meal, radio, push, setMeals);
     case '/bebidas':
-      return requestCocktail('cocktail', meal, radio, push);
+      return requestCocktail('cocktail', meal, radio, push, setMeals);
     default:
       return 'Erro';
   }
 };
 
 const SearchButton = () => {
-  const { searchType } = useContext(ComidasContext);
+  const { searchType, setMeals } = useContext(ComidasContext);
   const { type, meal, radio } = searchType;
   const { pathname } = useHistory().location;
   const { push } = useHistory();
@@ -54,7 +60,7 @@ const SearchButton = () => {
   return (
     <button
       data-testid="exec-search-btn"
-      onClick={() => requestType(type, meal, radio, pathname, push)}
+      onClick={() => requestType(type, meal, radio, pathname, push, setMeals)}
     >Buscar
     </button>
   );
